5d7ee1d6d49d8b8346a3cee02ab86d2444d252e3
Hao Shi <Steve@Haos-MacBook-Pro-2.local>
2025-06-03 20:08:10 -0400

    update spec

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Backend service started!"', () => {
+      expect(appController.getHello()).toBe('Backend service started!');
     });
   });
 });
